b001f139af8108054e7c65a972efacf0f6d903cd
danimfernandes <dani.mag.fernandes@gmail.com>
2021-05-19 12:05:44 +0200

    Create individualisePlinks.R

---
+++ helpers/individualisePlinks.R
@@ -0,0 +1,18 @@
+### Extract and convert all samples from binary PLINK dataset into individual text-PLINK
+individualisePlinks = function(dataset) {
+  # Description of arguments #
+  # dataset = Target dataset name without extensions
+  
+  dataLoad=read.table(paste0(dataset,".fam"), header=FALSE, sep="",as.is=TRUE, stringsAsFactors = FALSE)
+  it=1
+  for(i in dataLoad$V2) {
+    to_write=paste(dataLoad[it,1],i)
+    write.table(to_write,"to_keep",quote=FALSE,col.names = FALSE,row.names = FALSE)
+    cmd1=paste0("plink --bfile ",dataset," --keep to_keep --keep-allele-order --recode --out ",i)
+    system(cmd1)
+    it=it+1
+  }
+  unlink("to_keep")
+}
+
+individualisePlinks(dataset = "v443_1240K_forKinship")
