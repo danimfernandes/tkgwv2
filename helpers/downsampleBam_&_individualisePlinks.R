05eee49c872ad0cd132a682f0513e97c9d9f0dde
danimfernandes <dani.mag.fernandes@gmail.com>
2021-05-19 11:47:50 +0200

    Update downsampleBam_&_individualisePlinks.R

--- helpers/downsampleBam_&_individualisePlinks.R
+++ helpers/downsampleBam_&_individualisePlinks.R
@@ -1,12 +1,14 @@
 ### Downsize BAM files for faster analysis
-downsampleBAM = function(downsampleN = 1500000, extensionBam = "\\.bam", extensionDownBam = "_subsampled.bam") {
+downsampleBAM = function(downsampleN = 1500000, extensionBam = "\\.bam$", extensionDownBam = "_subsampled.bam") {
   # Description of arguments #
   # downsampleN = Default 1500000. Maximum number of reads to downsample BAM files to
   # extensionBam = Default "\\.bam". Work on BAM files with this extension/suffix
   # extensionDownBam = Default "_subsampled.bam". Extenstion/suffix for new downsampled BAM files
+  #
+  # Make sure your BAMs are indexed
   
-  for(i in list.files(getwd(),pattern = "\\.bam$")) {
-    sid = strsplit(i,extensionBam)[[1]]
+  for(i in list.files(getwd(),pattern = extensionBam)) {
+    sid = strsplit(i,paste0(extensionBam,"$"))[[1]]
     comm0 = paste0("samtools idxstats ",i," | awk {'print $3'}")
     mapLen = sum(as.integer(system(comm0, intern=T)))
     rat = downsampleN/mapLen
@@ -39,5 +41,5 @@ individualisePlinks = function(dataset) {
 }
 
 # Edit these lines and (un)comment to run the wanted functions
-downsampleBAM(downsampleN = 1500000, extensionBam = "\\.bam", extensionDownBam = "_subsampled.bam")
+downsampleBAM(downsampleN = 1500000, extensionBam = "\\.bam$", extensionDownBam = "_subsampled.bam")
 individualisePlinks(dataset = "v443_1240K_forKinship")
